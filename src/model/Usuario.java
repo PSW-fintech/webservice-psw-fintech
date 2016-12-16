bbd39bc3470a2a09175044ab05371758aee48c17
JorgeLuisXS <oojlxavier@gmail.com>
2016-12-16 01:43:45 +0000

    adicionadas pequenas clausulas de excecao na criacao de um usuario

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -42,14 +42,23 @@ public abstract class Usuario {
 	public Usuario(Map<String,Object> userData) throws Exception{
 		try{
 			this.endereco =(Endereco) userData.get("endereco") ;
+			this.setEndereco(this.endereco);
 			this.nome_completo =(String) userData.get("nome_completo") ;
+			this.setNome_completo(this.nome_completo);
 			this.codigo_pessoa =(String) userData.get("codigo_pessoa") ;
+			this.setCodigo_pessoa(this.codigo_pessoa);
 			this.telefone =(String) userData.get("telefone") ;
+			this.setTelefone(this.telefone);
 			this.telefone_opcional =(String) userData.get("telefone_opcional") ;
+			this.setTelefone_opcional(this.telefone_opcional);
 			this.email =(String) userData.get("email") ;
+			this.setEmail(this.email);
 			this.nome_do_usuario =(String) userData.get("nome_do_usuario") ;
+			this.setNome_do_usuario(this.nome_do_usuario);
 			this.senha_para_acesso =(String) userData.get("senha_para_acesso") ;
+			this.setSenha_para_acesso(this.senha_para_acesso);
 			this.foto_perfil =(String) userData.get("foto_perfil") ;
+			this.setFoto_perfil(this.foto_perfil);
 		}catch(Exception e){
 			throw e;
 		}
@@ -61,50 +70,85 @@ public abstract class Usuario {
 	public String getNome_completo() {
 		return nome_completo;
 	}
-	public void setNome_completo(String nome_completo) {
+	public void setNome_completo(String nome_completo)  throws Exception{
+		if(nome_completo == null || nome_completo.length() < 5){
+			throw new Exception();
+		}
 		this.nome_completo = nome_completo;
 	}
 	public String getCodigo_pessoa() {
 		return codigo_pessoa;
 	}
-	public void setCodigo_pessoa(String codigo_pessoa) {
+	public void setCodigo_pessoa(String codigo_pessoa)  throws Exception{
+		if(codigo_pessoa == null || codigo_pessoa.length() < 5){
+			throw new Exception();
+		}
 		this.codigo_pessoa = codigo_pessoa;
 	}
 	public String getTelefone() {
 		return telefone;
 	}
-	public void setTelefone(String telefone) {
+	public void setTelefone(String telefone)  throws Exception{
+		if(telefone == null || telefone.length() < 5){
+			throw new Exception();
+		}
 		this.telefone = telefone;
 	}
 	public String getTelefone_opcional() {
 		return telefone_opcional;
 	}
-	public void setTelefone_opcional(String telefone_opcional) {
+	public void setTelefone_opcional(String telefone_opcional)  throws Exception{
+		if(telefone_opcional == null || telefone_opcional.length() < 5){
+			throw new Exception();
+		}
 		this.telefone_opcional = telefone_opcional;
 	}
 	public String getEmail() {
 		return email;
 	}
-	public void setEmail(String email) {
+	public void setEmail(String email)  throws Exception{
+		if(email == null || email.length() < 5){
+			throw new Exception();
+		}
 		this.email = email;
 	}
 	public String getNome_do_usuario() {
 		return nome_do_usuario;
 	}
-	public void setNome_do_usuario(String nome_do_usuario) {
+	public void setNome_do_usuario(String nome_do_usuario)  throws Exception{
+		if(nome_do_usuario == null || nome_do_usuario.length() < 5){
+			throw new Exception();
+		}
 		this.nome_do_usuario = nome_do_usuario;
 	}
 	public String getSenha_para_acesso() {
 		return senha_para_acesso;
 	}
-	public void setSenha_para_acesso(String senha_para_acesso) {
+	public void setSenha_para_acesso(String senha_para_acesso)  throws Exception{
+		if(senha_para_acesso == null || senha_para_acesso.length() < 5){
+			throw new Exception();
+		}
 		this.senha_para_acesso = senha_para_acesso;
 	}
 	public String getFoto_perfil() {
 		return foto_perfil;
 	}
-	public void setFoto_perfil(String foto_perfil) {
+	public void setFoto_perfil(String foto_perfil)  throws Exception{
+		if(foto_perfil == null || foto_perfil.length() < 5){
+			throw new Exception();
+		}
 		this.foto_perfil = foto_perfil;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco)  throws Exception{
+		if(endereco == null){
+			throw new Exception();
+		}
+		this.endereco = endereco;
 	}
 	
 	
