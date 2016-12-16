f940af60b5e7bb744e0838a46b21f9323e2b72ac
JorgeLuisXS <oojlxavier@gmail.com>
2016-12-16 01:47:02 +0000

    adicionada clausulas de excecao pra pessoa fisica

--- src/model/PessoaFisica.java
+++ src/model/PessoaFisica.java
@@ -15,13 +15,19 @@ public class PessoaFisica extends Usuario{
 	public String getNome_da_mae() {
 		return nome_da_mae;
 	}
-	public void setNome_da_mae(String nome_da_mae) {
+	public void setNome_da_mae(String nome_da_mae) throws Exception{
+		if(nome_da_mae == null || nome_da_mae.length() < 5){
+			throw new Exception();
+		}
 		this.nome_da_mae = nome_da_mae;
 	}
 	public String getData_de_nascimento() {
 		return data_de_nascimento;
 	}
-	public void setData_de_nascimento(String data_de_nascimento) {
+	public void setData_de_nascimento(String data_de_nascimento) throws Exception{
+		if(data_de_nascimento == null || data_de_nascimento.length() < 5){
+			throw new Exception();
+		}
 		this.data_de_nascimento = data_de_nascimento;
 	}
 	
