ab27625a616d5c5b7b6b469f4be092ffed2b8d46
JorgeLuisXS <oojlxavier@gmail.com>
2016-12-21 17:09:12 +0000

    desevolvidos PagamentoRepository e InvestimentoRepository

---
+++ src/repository/InvestimentoRepository.java
@@ -0,0 +1,58 @@
+package repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import model.Investimento;
+
+
+public class InvestimentoRepository  {
+	EntityManagerFactory emf;
+	EntityManager em;
+	
+	public InvestimentoRepository(){
+		emf = Persistence.createEntityManagerFactory("fintech-PSW-MySQL");
+		em = emf.createEntityManager();
+	}
+	
+	public void criarNovoInvestimetno(Investimento investimento) throws Exception{
+		try{
+			em.getTransaction().begin();
+			em.merge(investimento);
+			em.getTransaction().commit();
+		}catch(Exception e){
+			throw e;
+		}
+	}
+	
+	public Investimento recuperarProjetoPorID(int idInvestimento) throws Exception{
+		try{
+			em.getTransaction().begin();
+			Investimento investimento = em.find(Investimento.class, idInvestimento);
+			em.getTransaction().commit();
+			return investimento;		
+		}catch(Exception e){
+			throw e;
+		}
+	}
+	
+	
+	public List<Investimento> recuperarTodosOsProjetos() throws Exception{
+		List<Investimento> investimentos;
+		try{
+			em.getTransaction().begin();
+			investimentos = (List<Investimento>) em.createQuery("SELECT investimentos FROM Investimento investimentos")
+					.getResultList();
+			em.getTransaction().commit();
+		}catch(Exception e){
+			throw e;
+		}
+		return investimentos;
+	}
+	
+	
+	
+}
---
+++ src/repository/PagamentoRepository.java
@@ -0,0 +1,58 @@
+package repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import model.Pagamento;
+
+
+public class PagamentoRepository  {
+	EntityManagerFactory emf;
+	EntityManager em;
+	
+	public PagamentoRepository(){
+		emf = Persistence.createEntityManagerFactory("fintech-PSW-MySQL");
+		em = emf.createEntityManager();
+	}
+	
+	public void criarNovoInvestimetno(Pagamento pagamento) throws Exception{
+		try{
+			em.getTransaction().begin();
+			em.merge(pagamento);
+			em.getTransaction().commit();
+		}catch(Exception e){
+			throw e;
+		}
+	}
+	
+	public Pagamento recuperarProjetoPorID(int idPagamento) throws Exception{
+		try{
+			em.getTransaction().begin();
+			Pagamento pagamento = em.find(Pagamento.class, idPagamento);
+			em.getTransaction().commit();
+			return pagamento;		
+		}catch(Exception e){
+			throw e;
+		}
+	}
+	
+	
+	public List<Pagamento> recuperarTodosOsProjetos() throws Exception{
+		List<Pagamento> Pagamentos;
+		try{
+			em.getTransaction().begin();
+			Pagamentos = (List<Pagamento>) em.createQuery("SELECT pagamentos FROM Pagamento pagamentos")
+					.getResultList();
+			em.getTransaction().commit();
+		}catch(Exception e){
+			throw e;
+		}
+		return Pagamentos;
+	}
+	
+	
+	
+}
